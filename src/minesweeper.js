3c4c441be29e10b72b76cc85a2771b9af196ca58
Ryan Lott <toranlyt@gmail.com>
2017-11-19 21:06:39 -0600

    MineSweeperGame Step 3.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,49 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-};
-const board = [[' ', ' ', ' '],
-               [' ', ' ', ' '],
-               [' ', ' ', ' ']
-              ];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
 
-printBoard(board);
-board[0][1] = 1;
-board[2][2] = 'B';
-printBoard(board);
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+    const row = [];
+
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+      const column = " ";
+      row.push(column);
+    }
+    board.push(row);
+  }
+  return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let bombBoard = [];
+
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+    const row = [];
+
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+      const column = null;
+      row.push(column);
+    }
+    bombBoard.push(row);
+  }
+
+  let numberOfBombsPlaced = 0;
+  while(numberOfBombsPlaced < numberOfBombs){
+    //Can accidentally place a bomb in the same place
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    bombBoard[randomRowIndex][randomColumnIndex] = "B";
+    numberOfBombsPlaced++;
+  }
+  return bombBoard;
+}
+
+const printBoard = board => board.map(row => row.join(' | ')).join('\n');
+
+const playerBoard = generatePlayerBoard(3,4);
+const bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: ');
+console.log(printBoard(playerBoard));
+
+console.log('Bomb Board: ');
+console.log(printBoard(bombBoard));
